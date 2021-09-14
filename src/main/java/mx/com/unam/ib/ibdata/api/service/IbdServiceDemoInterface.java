1c1e7a0463e919523c190d67e4b0811f73af4aac
cecamachocr@outlook.com <cecamachocr@outlook.com>
2021-09-13 23:12:39 -0500

    FIX - refactor packages

--- src/main/java/mx/com/unam/ib/ibdata/api/service/IbdServiceDemoInterface.java
+++
@@ -1,8 +0,0 @@
-package mx.com.unam.ib.ibdata.api.service;
-
-import mx.com.unam.ib.ibdata.api.model.vo.IbdUserVO;
-
-public interface IbdServiceDemoInterface {
-	
-	public IbdUserVO findUserByName(IbdUserVO req) throws Exception;
-}
--- src/main/java/mx/com/unam/ib/ibdata/api/utils/IbdContants.java
+++
@@ -1,8 +0,0 @@
-package mx.com.unam.ib.ibdata.api.utils;
-
-public class IbdContants {
-	
-	public static final String BASE_PATH_V1 = "/api-v1";
-	
-	public static final String BASE_PATH_M1 = "/demo";
-}
